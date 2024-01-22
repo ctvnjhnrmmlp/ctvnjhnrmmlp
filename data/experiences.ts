2ef97b9c3a2eefa800b6fc6a6e209953e1bd767b
--global <johnrommel.octaviano@gmail.com>
2024-01-22 18:05:41 +0800

    feat: updated working experience duration

--- data/experiences.ts
+++ data/experiences.ts
@@ -6,7 +6,14 @@ const experiences = [
 		mode: 'Remote',
 		description: `I have consistently excelled in the development and maintenance of Decentralized Applications (dApps) and Web3 games, showcasing my mastery of React JS, Next JS, and TailwindCSS to craft stunning, responsive user interfaces. To facilitate real-time communication and data retrieval, I harnessed the power of SocketIO, GraphQL, Apollo, and Rest APIs, thereby enhancing the seamless user experience within these cutting-edge applications. Leveraging Visual Studio Code and Git, I ensured efficient coding, version control, and collaborative workflows. My unwavering passion and adaptability have proven to be invaluable assets in navigating the ever-evolving landscape of decentralized applications and web3 games. In this role, I've continuously evolved and pushed the boundaries of my contributions, driving success and innovation within the team.`,
 		url: 'psi.com',
-		part: 2,
+	},
+	{
+		position: 'Front End Developer',
+		company: 'Konsumer Solutions Group',
+		duration: 'July 2023 - Present',
+		mode: 'Remote',
+		description: `I significantly contributed to the improvement of web products using cutting-edge technologies, including ReactJS, NextJS, TypeScript, and TailwindCSS. My work focused on enhancing user experience and overall functionality. I designed responsive and interactive interfaces with reusable React components, integrated NextJS for improved performance and SEO, and ensured codebase maintainability through TypeScript. My expertise in React and related technologies facilitated efficient team collaboration, problem-solving, and communication. I also kept up with industry trends, contributing positively to the team's efforts.`,
+		url: 'https://konsumersolutions.com',
 	},
 	{
 		position: 'Full Stack Developer',
@@ -15,17 +22,8 @@ const experiences = [
 		mode: 'Remote',
 		description: `I was instrumental in enhancing our ECommerce website's functionality and performance. Leveraging my skills in Bubble.io (No code), HTML, CSS, and JavaScript, I delivered responsive and visually appealing web interfaces that improved the user experience and increased conversions. I excelled in building and optimizing REST APIs, utilizing Postman for thorough testing and validation. This ensured seamless data communication with third-party services and contributed to the website's reliability and efficiency. My experience with Agile Methodology allowed me to work in cross-functional teams, ensuring a collaborative and iterative development process. I consistently met project milestones, delivering high-quality features and updates on schedule. My contributions extended beyond code.`,
 		url: 'raicom.com',
-		part: 2,
 	},
-	{
-		position: 'Front End Developer',
-		company: 'Konsumer Solutions Group',
-		duration: 'July 2023 - September 2023',
-		mode: 'Remote',
-		description: `I significantly contributed to the improvement of web products using cutting-edge technologies, including ReactJS, NextJS, TypeScript, and TailwindCSS. My work focused on enhancing user experience and overall functionality. I designed responsive and interactive interfaces with reusable React components, integrated NextJS for improved performance and SEO, and ensured codebase maintainability through TypeScript. My expertise in React and related technologies facilitated efficient team collaboration, problem-solving, and communication. I also kept up with industry trends, contributing positively to the team's efforts.`,
-		url: 'https://konsumersolutions.com',
-		part: 2,
-	},
+
 	{
 		position: 'Full Stack Developer',
 		company: 'QX Information Technology Corp',
@@ -33,7 +31,6 @@ const experiences = [
 		mode: 'Remote',
 		description: `I am a highly adaptable Full Stack Developer skilled in a wide range of technologies including WordPress, PHP, Laravel, Angular, JavaScript, HTML, CSS, Bootstrap, Sass, and various industry-standard tools like Figma, VS Code, Photoshop, and Git. With a focus on PHP and Laravel, I excel in developing robust backend solutions with intricate functionalities and database structures. My proficiency in Angular, JavaScript, HTML, CSS, and Bootstrap allows me to create engaging and responsive user interfaces for cross-platform experiences. I have consistently contributed to project success by delivering dynamic and visually appealing web applications with seamless user experiences and robust functionality. I work closely with cross-functional teams to gather requirements and offer technical expertise, consistently meeting deadlines.`,
 		url: 'https://www.quantumx.com/',
-		part: 1,
 	},
 	{
 		position: 'Front End Developer',
@@ -42,7 +39,6 @@ const experiences = [
 		mode: 'Remote',
 		description: `I honed my expertise in crafting exceptional user interfaces and improving user experiences through the use of HTML, CSS, and JavaScript. My work involved integrating data-driven elements and implementing effective CRM solutions, resulting in enhanced user engagement and optimized business processes. I demonstrated proficiency in tools like iHouseCRM, Excel, and Forms, and I streamlined development processes using industry-standard tools like VS Code and Zoho, ensuring the timely delivery of high-quality front-end solutions. I excelled in addressing client requests and ensured their websites were up-to-date and optimized for peak performance. My comprehensive website maintenance skills, coupled with meticulous record-keeping using a CRM system, played a crucial role in maintaining and enhancing website functionality.`,
 		url: 'https://www.growwithfusion.com/',
-		part: 1,
 	},
 	{
 		position: 'Graphic Designer',
@@ -51,7 +47,6 @@ const experiences = [
 		mode: 'Remote',
 		description: `I have gained extensive experience in creating visually captivating designs for various projects, leveraging my expertise in technologies such as Canva, Figma, and Photoshop. With an eye for aesthetics, a deep understanding of design principles, and a dedication to staying updated with emerging trends, I consistently deliver high-quality graphics that engage and captivate audiences across different platforms. My diverse skill set, proficiency in design tools, and commitment to continuous learning make me well-equipped to bring exceptional visual solutions.`,
 		url: 'https://www.facebook.com/DogBreeder2022/',
-		part: 1,
 	},
 ];
 
