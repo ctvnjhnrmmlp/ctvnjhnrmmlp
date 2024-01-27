3aed1e18fc84b434f323c49571f0618c4ec60fa7
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2024-01-28 00:35:21 +0800

    fix: refactored Tailwind config by adding more content entries

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,7 @@ const { nextui } = require('@nextui-org/react');
 
 const config: Config = {
 	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./layouts/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./(app|components|containers|layouts)/**/*.{js,ts,jsx,tsx,mdx}',
 		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
