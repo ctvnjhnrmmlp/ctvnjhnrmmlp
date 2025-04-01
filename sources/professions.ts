e934364768017602bdc6f49bafe75f734bf7c2e8
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-01 22:31:31 +0800

    fix: updated professions

--- sources/professions.ts
+++ sources/professions.ts
@@ -1,24 +1,24 @@
 const PROFESSIONS = [
-	{
-		name: 'Cloud',
-		slug: 'cloud',
-	},
-	{
-		name: 'DApps',
-		slug: 'dapps',
-	},
-	{
-		name: 'Mobile',
-		slug: 'mobile',
-	},
-	{
-		name: 'Smart Contracts',
-		slug: 'smart-contracts',
-	},
-	{
-		name: 'Web',
-		slug: 'web',
-	},
+  {
+    name: 'Blockchain',
+    slug: 'blockchain',
+  },
+  {
+    name: 'Cloud',
+    slug: 'cloud',
+  },
+  {
+    name: 'Data',
+    slug: 'data',
+  },
+  {
+    name: 'Mobile',
+    slug: 'mobile',
+  },
+  {
+    name: 'Web',
+    slug: 'web',
+  },
 ];
 
 export default PROFESSIONS;
