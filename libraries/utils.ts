e385b94c33c410165d2a1168cb93a548f33d2f13
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-01 23:37:53 +0800

    feat: added utility functions

--- libraries/utils.ts
+++ libraries/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getJoinedStringComma(text: string[]) {
+  return text.join(', ');
+}
+
+export function getJoinedStringPeriod(text: string[]) {
+  return text.join('. ');
+}
+
+export function getTotalWorkingExperience() {
+  const startDate = new Date('2023-03-06');
+  const currentDate = new Date();
+  // @ts-ignore
+  const timeDifference = currentDate - startDate;
+  const millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
+  const totalYears = timeDifference / millisecondsInYear;
+  const formattedTotalYears = totalYears.toFixed(2);
+
+  return formattedTotalYears;
 }
