1c77035ce157b97f95301c5c3147d9935b8a5a16
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-01 22:45:11 +0800

    fix: updated site logo

--- configurations/variables.ts
+++ configurations/variables.ts
@@ -1,15 +1,15 @@
 const VARIABLES = {
-	canonical: 'https://ctvnjhnrmmlp.vercel.app',
-	name: 'John Rommel Octaviano',
-	nameConfig: 'ctvnjhnrmmlp',
-	developers: 'John Rommel Octaviano',
-	stacks: 'typescript, react, nextjs, tailwindcss',
-	words: 'john rommel octaviano, ctvnjhnrmmlp',
-	info: 'Software Engineer | Applications, Machine Learning, Blockchain, DevOps, Cloud',
-	icon: 'self-logo.svg',
-	image: 'self-abstract.webp',
-	theme: 'dark',
-	robots: 'index, follow',
+  canonical: 'https://ctvnjhnrmmlp.vercel.app',
+  name: 'John Rommel Octaviano',
+  nameConfig: 'ctvnjhnrmmlp',
+  developers: 'John Rommel Octaviano',
+  stacks: 'typescript, react, nextjs, tailwindcss',
+  words: 'john rommel octaviano, ctvnjhnrmmlp',
+  info: 'Software Engineer | Applications, Machine Learning, Blockchain, DevOps, Cloud',
+  icon: 'self-logo.png',
+  image: 'self-abstract.webp',
+  theme: 'dark',
+  robots: 'index, follow',
 };
 
 export default VARIABLES;
