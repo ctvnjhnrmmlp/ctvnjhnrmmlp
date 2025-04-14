203d198c7f8170a8828e341af9f71d1d9e34227d
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-14 12:27:50 +0800

    fix: updated github api endpoint

--- app/api/repos/route.ts
+++ app/api/repos/route.ts
@@ -2,12 +2,15 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await fetch(`https://api.github.com/user/repos`, {
-      headers: {
-        'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    });
+    const response = await fetch(
+      `https://api.github.com/users/ctvnjhnrmmlp/repos`,
+      {
+        headers: {
+          'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      }
+    );
 
     if (response.ok) {
       const data = await response.json();
