55e6b2569ff6503d6f81479700f6b3add993c825
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-14 10:57:49 +0800

    feat: added github api routes

---
+++ app/api/repos/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const response = await fetch(`https://api.github.com/user/repos`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+
+      return NextResponse.json(
+        {
+          message: 'Repositories fetched.',
+          data,
+        },
+        { status: 200 }
+      );
+    }
+
+    throw new Error();
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
--- services/github/repositories.ts
+++ services/github/repositories.ts
@@ -1,9 +1,9 @@
 export class Repositories {
-  static endpoint = `${process.env.BASE_URL}`;
+  static endpoint = `${process.env.BASE_URL}/api`;
 
   static async readRepositories(): Promise<GitHubRepositories | null> {
     try {
-      const response = await fetch(`${this.endpoint}`);
+      const response = await fetch(`${this.endpoint}/repos`);
 
       if (response.ok) {
         const data = await response.json();
