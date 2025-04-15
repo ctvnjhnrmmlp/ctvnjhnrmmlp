5900504c22a07e3b1d6262fb818358e9d103173e
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-15 17:35:46 +0800

    fix: updated variables

--- configurations/variables.ts
+++ configurations/variables.ts
@@ -5,7 +5,7 @@ const VARIABLES = {
   developers: 'John Rommel Octaviano',
   stacks: 'typescript, react, nextjs, tailwindcss',
   words: 'john rommel octaviano, ctvnjhnrmmlp',
-  info: 'Software Engineer | Applications, Machine Learning, Blockchain, DevOps, Cloud',
+  info: 'Software Engineer | Blockchain, Cloud, Data, Mobile, Web',
   icon: 'self-logo.png',
   image: 'self-abstract.webp',
   theme: 'dark',
