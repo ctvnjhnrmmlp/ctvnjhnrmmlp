b4675338468e1493b3299a483feea49533c73a26
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-18 21:38:25 +0800

    fix: hide resume and cover

--- app/cover/page.tsx
+++ app/cover/page.tsx
@@ -3,12 +3,7 @@
 export default function Page() {
   return (
     <main className='flex items-center justify-center min-h-screen bg-black'>
-      <section className='w-full h-screen'>
-        <iframe
-          src='/documents/john-rommel-octaviano-software-engineer-cover-letter.pdf'
-          className='w-full h-full'
-        ></iframe>
-      </section>
+      <section className='w-full h-screen'></section>
     </main>
   );
 }
